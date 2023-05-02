4c2bea3b1d767dc99cc5bf165fce1507669d0d7b
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-05-02 05:13:29 -0600

    Completed a series of challenges

---
+++ Basic Commands.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+num1 = 4
+num2 = 5
+num3 = num1 + num2
+num3 
+print(num1)
+print(num2)
+print('num3 = num1 + num2')
+print(num3)
+fname = "MeLyssa"
+lname = "Stewart"
+
+
+print("Hello {} {}, welcom to Python." .format(fname, lname) )
+
+
---
+++ integers and operations.py
@@ -0,0 +1,12 @@
+num1 = 1 #integer
+num2 = 2 #integer
+
+print(num1 + num2)
+
+num3 = 1.6 #float
+num4 = 2.5 #float
+
+print (num3+num4)
+
+myString = "hello world!"
+len(myString) #counts the length
