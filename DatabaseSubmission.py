791e23b9fc822df61d66d0d7fa9e346bf5e3f061
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-07-06 14:20:28 -0600

    Update DatabaseSubmission.py

--- DatabaseSubmission.py
+++ DatabaseSubmission.py
@@ -15,16 +15,10 @@ conn.close
 ListName = ["information.docx", "Hello.txt", "myImage.png", \
          "myMovie.mpg", "World.txt", "data.pdf", "myPhoto.jpg"]
 
-txtList = []
-def query_string ():
-    for (let i = 0; i <= listName.length; i++) {
-    x = txt.find("*.txt")
-    txtList.append(x)
-    } 
-
-
-with conn:
-    cur = conn.cursor()
-    cur.execute(query_string)
-    conn.commit
-conn.close
+for x in ListName:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_filelist VALUES (?)", (x,))
+            print(x)
+conn.close()
